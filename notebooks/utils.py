33b6b26411426c6317ed6548a3e2940170954f07
Guillermo Antonio Fidalgo Rodriguez <gfidalgo@lxplus048.cern.ch>
2018-04-05 12:16:48 +0200

    first import for supervised model using keras

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -19,3 +19,98 @@ def get_data(file_name,group='EBOccupancyTask_EBOT_rec_hit_occupancy',data_type=
    ret_array=preprocess(ret_array,preprocess_level)
    logging.debug("Supplying "+str(ret_array.shape[0])+" samples")
    return ret_array
+
+
+def killregion(image,xdim,ydim):  
+    tempX=image.copy()
+    x1=np.min(xdim)
+    x2=np.max(xdim)
+    y1=np.min(ydim)
+    y2=np.max(ydim)
+    if x1==x2 | y1==y2:
+        print "error, no change is made on image"
+    #if tempX.ndim ==3:
+    #    
+    #    for k in range(tempX.shape[0]):
+    #        
+    #        for i in range(x1,x2):
+    #            
+    #            for j in range(y1,y2):
+    #                
+    #                tempX[k,j,i]=0
+    #
+    #else:
+        
+    for i in range(x1,x2):
+            
+        for j in range(y1,y2):
+                
+            tempX[j,i]=0
+                
+    return tempX
+
+
+#help (np.zeros)
+def hotregion(image,xdim,ydim):  
+    tempX=image.copy()
+    x1=np.min(xdim)
+    x2=np.max(xdim)
+    y1=np.min(ydim)
+    y2=np.max(ydim)
+    if x1==x2 | y1==y2:
+        print "error, no change is made on image"
+   # if tempX.ndim ==3:
+   #     
+   #     for k in range(tempX.shape[0]):
+   #         
+   #         for i in range(x1,x2):
+   #             
+   #             for j in range(y1,y2):
+   #                 
+   #                 tempX[k,j,i]=np.max(image)
+   # else:
+        
+    for i in range(x1,x2):
+            
+        for j in range(y1,y2):
+                
+            tempX[j,i]=np.max(image)
+                
+    return tempX
+
+def randomregion(image,xdim,ydim):
+    
+    
+    tempX=image.copy()
+    x1=abs(np.min(xdim))
+    x2=np.max(xdim)
+    y1=abs(np.min(ydim))
+    y2=np.max(ydim)
+    if x1==x2 | y1==y2:
+        print "error, no change is made on image"
+    #print "x1,x2,y1,y2",x1,x2,y1,y2
+    random_noise_region=np.random.randint(np.max(image), size=( abs(x2-x1), abs(y2-y1) ) )   
+    #print "random_noise_region.shape",random_noise_region.shape
+    
+
+  #  if tempX.ndim == 3:
+  #      
+  #      for k in range(tempX.shape[0]):
+  #          
+  #          for i in range(x1,x2):
+  #              
+  #              for j in range(y1,y2):
+  #                  te = i-x1
+  #                  te2= j-y1
+  #                  tempX[k,j,i]=random_noise_region[te,te2]
+  #  
+  #  else:
+  #      
+    for i in range(x1,x2):
+            
+        for j in range(y1,y2):
+            te = i-x1
+            te2= j-y1
+            tempX[j,i]=random_noise_region[te,te2]
+                
+    return tempX
